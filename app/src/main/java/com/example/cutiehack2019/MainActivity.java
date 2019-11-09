c95fd3f0de399ce0b65adc10022210f8a33f6f66
Alex Coats <swifteralex@users.noreply.github.com>
2019-11-09 12:55:04 -0800

    Added smoother movement

--- app/src/main/java/com/example/cutiehack2019/MainActivity.java
+++ app/src/main/java/com/example/cutiehack2019/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.cutiehack2019;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.graphics.Point;
-import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
-import android.view.Window;
 import android.widget.ImageView;
 
 import java.util.Timer;
@@ -26,7 +23,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private float vx = 0;
     private float vy = 0;
-    private int turtleDiameter = 100;
+    private int turtleDiameter = 80;
+
+    private ImageView[] trash = new ImageView[10];
+    private ImageView[] walls = new ImageView[100];
+    //private ImageView turtle = findViewById(R.id.turtle);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +48,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         final int FPS = 60;
         TimerTask updateFrame = new UpdateFrame();
         timer.scheduleAtFixedRate(updateFrame, 0, 1000/FPS);
+
+        createLevel();
+    }
+
+    public void gameOver() {
+        
+    }
+
+    public void createLevel() {
+        trash[0] = findViewById(R.id.trash1);
+        trash[1] = findViewById(R.id.trash2);
+        trash[2] = findViewById(R.id.trash3);
+        trash[3] = findViewById(R.id.trash4);
     }
 
     @Override
@@ -54,23 +68,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent){
-        vx -= sensorEvent.values[0]/3;
-        vy += sensorEvent.values[1]/3;
+        vx -= sensorEvent.values[0]/2;
+        vy += sensorEvent.values[1]/2;
     }
 
     public class UpdateFrame extends TimerTask {
         public void run(){
-            ImageView circle = findViewById(R.id.imageView);
 
-            if(circle.getX() + vx < 0 || circle.getX() + vx + turtleDiameter > screenWidth){
+            ImageView turtle = findViewById(R.id.turtle);
+
+            if(turtle.getX() + vx < 0 || turtle.getX() + vx + turtleDiameter > screenWidth){
                 vx *= -0.5;
             }
-            if(circle.getY() + vy < 0 || circle.getY() + vy + turtleDiameter > screenHeight) {
+            if(turtle.getY() + vy < 0 || turtle.getY() + vy + turtleDiameter > screenHeight) {
                 vy *= -0.5;
             }
+            for(int i=0; i<4; i++) {
+                if (turtle.getX() + turtleDiameter >= trash[i].getX() && turtle.getX() <= trash[i].getX() + 80){
+                    if (turtle.getY() + turtleDiameter >= trash[i].getY() && turtle.getY() <= trash[i].getY() + 80){
+                        vx *= 0;
+                        vy *= 0;
+                    }
+                }
+            }
 
-            circle.setX(circle.getX() + vx);
-            circle.setY(circle.getY() + vy);
+
+            turtle.setX(turtle.getX() + vx);
+            turtle.setY(turtle.getY() + vy);
+
+            int rotation = (int)((180/Math.PI)*Math.atan(vy/vx));
+            if(vx < 0){
+                rotation += 180;
+            }
+            turtle.setRotation(rotation - 90);
+
+
         }
     }
 }

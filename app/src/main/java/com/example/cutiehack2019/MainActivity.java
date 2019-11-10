1f2fe844c2fd597a6fa3ded30c7ee13a6809c675
Alex Coats <swifteralex@users.noreply.github.com>
2019-11-09 17:37:37 -0800

    Working state with full functionality

--- app/src/main/java/com/example/cutiehack2019/MainActivity.java
+++ app/src/main/java/com/example/cutiehack2019/MainActivity.java
@@ -29,9 +29,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float vy = 0;
     private int turtleDiameter = 80;
     private boolean _gameOver = false;
+    private int score = 0;
 
-    private ImageView[] trash = new ImageView[10];
-    private ImageView[] walls = new ImageView[100];
+    private ImageView[] trash = new ImageView[9];
+    private ImageView[] walls = new ImageView[22];
+    private ImageView[] jellys = new ImageView[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         display.getSize(size);
         screenWidth = size.x;
         screenHeight = size.y;
+
+        TextView gameOverText = findViewById(R.id.textView2);
+        TextView winnerText = findViewById(R.id.textView3);
+        Button gameOverButton1 = findViewById(R.id.button2);
+        Button gameOverButton2 = findViewById(R.id.button3);
+        winnerText.setX(20000);
+        gameOverText.setX(20000);
+        gameOverButton1.setX(20000);
+        gameOverButton2.setX(20000);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -58,6 +69,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void retry(View view) {
         ImageView turtle = findViewById(R.id.turtle);
+        TextView winnerText = findViewById(R.id.textView3);
+        TextView gameOverText = findViewById(R.id.textView2);
+        Button gameOverButton1 = findViewById(R.id.button2);
+        Button gameOverButton2 = findViewById(R.id.button3);
 
         int startingX = 0;
         int startingY = 0;
@@ -67,15 +82,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         turtle.setX(startingX);
         turtle.setY(startingY);
 
+        winnerText.setX(20000);
+        gameOverText.setX(20000);
+        gameOverButton1.setX(20000);
+        gameOverButton2.setX(20000);
+
         _gameOver = false;
     }
 
     public void gameOver() {
         _gameOver = true;
+        TextView gameOverText = findViewById(R.id.textView2);
+        Button gameOverButton1 = findViewById(R.id.button2);
+        Button gameOverButton2 = findViewById(R.id.button3);
+        gameOverText.setX(1080/2 - 125);
+        gameOverButton1.setX(1080/3 - 125);
+        gameOverButton2.setX(1080 * 2/3 - 125);
+
+        gameOverText.setY(1920/5);
+        gameOverButton1.setY(1920 * 3/5);
+        gameOverButton2.setY(1920 * 3/5);
     }
 
     public void win() {
         _gameOver = true;
+        TextView gameOverText = findViewById(R.id.textView3);
+        Button gameOverButton1 = findViewById(R.id.button2);
+        Button gameOverButton2 = findViewById(R.id.button3);
+        gameOverText.setX(1080/2 - 125);
+        gameOverButton1.setX(1080/3 - 125);
+        gameOverButton2.setX(1080 * 2/3 - 125);
+
+        gameOverText.setY(1920/5);
+        gameOverButton1.setY(1920 * 3/5);
+        gameOverButton2.setY(1920 * 3/5);
+    }
+    public void quit(View view){
+        System.exit(0);
     }
 
     public void createLevel() {
@@ -86,31 +129,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         trash[4] = findViewById(R.id.trash5);
         trash[5] = findViewById(R.id.trash6);
         trash[6] = findViewById(R.id.trash7);
-        trash[7] = findViewById(R.id.trash9);
+        trash[7] = findViewById(R.id.trash8);
+        trash[8] = findViewById(R.id.trash9);
 
+//        for(int i = 1; i <= trash.length; i++) {
+//            int id = getResources().getIdentifier("trash" + i, "id", getPackageName());
+//            trash[i-1] = (ImageView) findViewById(id);
+//        }
 
-        walls[0] = findViewById(R.id.wall1);
-        walls[1] = findViewById(R.id.wall2);
-        walls[2] = findViewById(R.id.wall3);
-        walls[3] = findViewById(R.id.wall4);
-        walls[4] = findViewById(R.id.wall5);
-        walls[5] = findViewById(R.id.wall6);
-        walls[6] = findViewById(R.id.wall7);
-        walls[7] = findViewById(R.id.wall8);
-        walls[8] = findViewById(R.id.wall9);
-        walls[9] = findViewById(R.id.wall10);
-        walls[10] = findViewById(R.id.wall11);
-        walls[11] = findViewById(R.id.wall12);
-        walls[12] = findViewById(R.id.wall13);
-        walls[13] = findViewById(R.id.wall14);
-        walls[14] = findViewById(R.id.wall15);
-        walls[15] = findViewById(R.id.wall16);
-        walls[16] = findViewById(R.id.wall17);
-        walls[17] = findViewById(R.id.wall18);
-        walls[18] = findViewById(R.id.wall19);
-        walls[19] = findViewById(R.id.wall20);
-        walls[20] = findViewById(R.id.wall21);
-        walls[21] = findViewById(R.id.wall24);
+        for(int i = 1; i <= 22; i++) {
+            int id = getResources().getIdentifier("wall" + i, "id", getPackageName());
+            walls[i-1] = (ImageView) findViewById(id);
+        }
+//        walls[0] = findViewById(R.id.wall1);
+//        walls[1] = findViewById(R.id.wall2);
+//        walls[2] = findViewById(R.id.wall3);
+//        walls[3] = findViewById(R.id.wall4);
+//        walls[4] = findViewById(R.id.wall5);
+//        walls[5] = findViewById(R.id.wall6);
+//        walls[6] = findViewById(R.id.wall7);
+//        walls[7] = findViewById(R.id.wall8);
+//        walls[8] = findViewById(R.id.wall9);
+//        walls[9] = findViewById(R.id.wall10);
+//        walls[10] = findViewById(R.id.wall11);
+//        walls[11] = findViewById(R.id.wall12);
+//        walls[12] = findViewById(R.id.wall13);
+//        walls[13] = findViewById(R.id.wall14);
+//        walls[14] = findViewById(R.id.wall15);
+//        walls[15] = findViewById(R.id.wall16);
+//        walls[16] = findViewById(R.id.wall17);
+//        walls[17] = findViewById(R.id.wall18);
+//        walls[18] = findViewById(R.id.wall19);
+//        walls[19] = findViewById(R.id.wall20);
+//        walls[20] = findViewById(R.id.wall21);
+//        walls[21] = findViewById(R.id.wall22);
     }
 
     @Override
@@ -140,7 +192,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             if(turtle.getY() + vy > screenHeight - 200){
                 win();
             }
-            for(int i=0; i<8; i++) {
+            for(int i=0; i < trash.length; i++) {
                 if (turtle.getX() + turtleDiameter >= trash[i].getX() && turtle.getX() <= trash[i].getX() + 80){
                     if (turtle.getY() + turtleDiameter >= trash[i].getY() && turtle.getY() <= trash[i].getY() + 80) {
                         if (vx != 0) {
@@ -149,10 +201,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     }
                 }
             }
+            for(int i = 0; i < jellys.length; i++) {
+                if (turtle.getX() + turtleDiameter >= jellys[i].getX() && turtle.getX() <= jellys[i].getX() + 80){
+                    if (turtle.getY() + turtleDiameter >= jellys[i].getY() && turtle.getY() <= jellys[i].getY() + 80) {
+                        if (vx != 0) {
+                            
+                        }
+                    }
+                }
+            }
 
             try {
                 int threshold = 10;
-                for (int i = 0; i < 22; i++) {
+                for (int i = 0; i < walls.length; i++) {
                     if (turtle.getX() + vx + turtleDiameter >= walls[i].getX() && turtle.getX() + vx <= walls[i].getX() + 100) {
                         if (turtle.getY() + vy + turtleDiameter >= walls[i].getY() && turtle.getY() + vy <= walls[i].getY() + 100) {
                             if (turtle.getX() + turtleDiameter <= walls[i].getX() && turtle.getX() + vx + turtleDiameter >= walls[i].getX() ||
